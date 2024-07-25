35df77999e0553a74eaa169ddd826867adc1a55c
Michael Fischer <michael_fischer_2@sfu.ca>
2024-07-24 20:07:24 -0700

    added runtime vs profits comparison

--- factors_affecting_movies_profitability.py
+++ factors_affecting_movies_profitability.py
@@ -1,13 +1,20 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy import stats
 from IPython.display import display
 
 class MovieFactors:
     def __init__(self):
         self.ratings = np.linspace(0,10,21)
         self.data = self.get_data()
+        self.clean_data_more()
         self.data_grouped_by_ratings = self.group_data_by_rating()
+        
+    def clean_data_more(self):
+        self.data['runtimeMinutes'] = pd.to_numeric(self.data['runtimeMinutes'], errors='coerce').dropna()
+        self.data = self.data[self.data['runtimeMinutes'] >= 0]
+
 
     def get_data(self):
         return pd.read_csv("movie_dataset_cleaned_with_profits.csv", index_col = "tconst")
@@ -18,8 +25,26 @@ class MovieFactors:
         plt.xlabel('Rating')
         plt.ylabel('Profitability in millions')
         plt.scatter(self.data['averageRating'], self.data['profit']/1000000, color = 'blue', alpha = 0.5)
+        self.data['linearRating'] = self.data['averageRating']
+        fit = stats.linregress(self.data['linearRating'], self.data['profit'] /1000000)
+        plt.plot(self.data['averageRating'], self.data['linearRating'] *fit.slope + fit.intercept, color = 'red', linewidth = 3)
         plt.show()
         # plt.savefig('test2.png')
+
+    def scatter_plot(self, title, xlabel, ylabel, scat_x, scat_y, lin_name, lin_copy, lin_column, show):
+        plt.figure(figsize=(8,5))
+        plt.title(title)
+        plt.xlabel(xlabel)
+        plt.ylabel(ylabel)
+        plt.scatter(scat_x, scat_y, color = 'blue', alpha = 0.5)
+        self.data[lin_name] = self.data[lin_copy]
+        fit = stats.linregress(self.data[lin_name], lin_column)
+        print(fit)
+        plt.plot(self.data[lin_name], self.data[lin_name] *fit.slope + fit.intercept, color = 'red', linewidth = 3)
+        if (show):
+            plt.show()
+        else:
+            plt.savefig('test2.png')
 
     def group_data_by_rating(self):
         temp = self.data.copy(deep=True)
@@ -46,5 +71,24 @@ class MovieFactors:
 if __name__ == "__main__":
     movies = MovieFactors()
     # movies.scatter_plot_ratings_vs_profitability()
+    # movies.scatter_plot('Movies Ratings vs Profitability', 'Rating', 'Profitability in millions', 
+    #                     movies.data['averageRating'], movies.data['profit']/1000000, 
+    #                     'linearRating', 'averageRating', movies.data['profit'] /1000000, True )
+    
+    # movies.data = movies.data[movies.data['runtimeMinutes'] <= 250]
+    # movies.data = movies.data[movies.data['profit']/1000000 <= 500]
+
+    # TODO
+    # maybe do loess smoothing or something?
+
+    movies.scatter_plot('Movies Runtime vs Profitability', 'Run Time', 'Profitability in millions', 
+                        movies.data['runtimeMinutes'], movies.data['profit']/1000000, 
+                        'linearRuntime', 'runtimeMinutes', movies.data['profit'] /1000000000, True )
+    # movies.data['linearRuntime'] = movies.data['runtimeMinutes']
+    # movies.data['linearRating'] = movies.data['averageRating']
+    # movies.data['profit_in_millions'] = movies.data['profit'] / 1000000000
+    # fit = stats.linregress(movies.data['linearRuntime'], movies.data['profit_in_millions'])
+    # print(fit)
+
     # movies.histogram_ratings_vs_profitability()
-    display(movies.data.sort_values(by="startYear"))
+    # display(movies.data.sort_values(by="startYear"))
