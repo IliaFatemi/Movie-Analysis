988dcf1f017318b06dbfbb8f22fd453d9ad5146b
Michael Fischer <michael_fischer_2@sfu.ca>
2024-07-29 00:44:47 -0700

    Movies Ratings vs Median Profits Graph
    Graph of Movies Ratings vs Median Profits
    Number of entries per ratings group included
    Excluded ratings < 40 entries in bar graph.
    Created Print for median of multiple categories and calculated standard deviation / mean for rating.

--- factors_affecting_movies_profitability.py
+++ factors_affecting_movies_profitability.py
@@ -3,14 +3,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy import stats
 from IPython.display import display
+from sklearn.impute import KNNImputer
 from statsmodels.nonparametric.smoothers_lowess import lowess
 
 class MovieFactors:
     def __init__(self):
-        self.ratings = np.linspace(0,10,21)
         self.data = self.get_data()
         self.clean_data_more()
-        self.data_grouped_by_ratings = self.group_data_by_rating()
         
     def clean_data_more(self):
         self.data['profit'] = self.data['profit'] / 1000000 # 1 mil
@@ -42,7 +41,8 @@ class MovieFactors:
             self.data[lin_name] = self.data[lin_copy]
             fit = stats.linregress(self.data[lin_name], lin_column)
             print(fit)
-            # plt.plot(self.data[lin_name], self.data[lin_name] *fit.slope + fit.intercept, color = 'red', linewidth = 3)
+            plt.plot(self.data[lin_name], self.data[lin_name] *fit.slope + fit.intercept, color = 'red', linewidth = 3)
+
         if show_loess:
             loess = lowess(scat_y, scat_x, frac = 0.2, it = 300)
             plt.plot(loess[:, 0], loess[:, 1], 'g-')
@@ -51,84 +51,134 @@ class MovieFactors:
         else:
             plt.savefig('test2.png')
 
-    def group_data_by_rating(self):
+    def Histogram_Ratings_vs_Profits(self, show):
+        rating_groups = np.linspace(1,10,19)
+
+        plt.figure(figsize=(10,7))
+        plt.title('Movies Ratings vs Median Profits', fontsize=16)
+        plt.xlabel('Rating', fontsize=14)
+        plt.xticks(rating_groups[:-1])
+        plt.ylabel('Value', fontsize=14)
+
+        ## GROUP DATA BY RATING
         temp = self.data.copy(deep=True)
-        temp['rating_groups'] = pd.cut(temp['averageRating'], self.ratings) 
+        temp['rating_groups'] = pd.cut(temp['averageRating'], rating_groups) 
         # Referenced this webpage for how to use the cut function to give a new ratings column that will be used to group data
         # https://pandas.pydata.org/docs/reference/api/pandas.cut.html
 
-        # take sum so we get the profitability of all entries in the groupings
-        return temp.groupby('rating_groups', observed = False).sum().reset_index() # set observed to false remove warning
+        grouped = temp.groupby('rating_groups', observed = False).agg(
+            profit = pd.NamedAgg(column='profit', aggfunc='median'),
+            entries = pd.NamedAgg(column='profit', aggfunc='count'),
+        ).reset_index() # set observed to false remove warning
+        grouped['groups'] = rating_groups[:-1] # create groups column for merging, excludes last index of value 10
 
-    def histogram_ratings_vs_profitability(self):
-        plt.figure(figsize=(10,7))
-        plt.title('Movies Ratings vs Profitability')
-        plt.xlabel('Rating')
-        plt.xticks(rotation=25)
-        plt.ylabel('Profitability in millions')
-        plt.bar(self.data_grouped_by_ratings['rating_groups'].astype(str), self.data_grouped_by_ratings['profit'], width=0.5, edgecolor = "black")
+        ## INCONCLUSIVE GROUPED DATA
+        null_groups = pd.DataFrame(rating_groups[:-1], columns=["groups"])
+        less_than_20 = grouped[grouped['entries'] < 40] 
+        # rating groups with less than 20 are excluded from bar graph
+        null_groups = pd.merge(null_groups, less_than_20, on = "groups", how="outer")
+        null_groups.loc[null_groups['entries'].isnull() , 'profit'] = 0
+        null_groups.loc[null_groups['entries'].isnull() == False, 'profit'] = 1
+        # Referenced this for how to alter data in rows depending on a condition
+        # https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.loc.html
+        
+        ## ENTRIES PER RATING GROUP AS A PERCENTAGE SCALED BY 15
+        grouped.loc[grouped['entries'] < 40, 'profit'] = 0
+        total_entries = self.data.shape[0]
+        grouped['entries_perc_times_200'] = (grouped['entries'] / total_entries) * 15
+
+        # # -- Can uncomment to see raw data on group pins --
+        # pd.set_option('display.max_rows', None)
+        # display(grouped)
+        
+        plt.bar(grouped['groups'] + 0.25, grouped['profit'], width=0.5, edgecolor = "black") # profits
+        plt.bar(rating_groups[:-1]+ 0.25, null_groups['profit'],width=0.5, color='none', hatch='/', edgecolor = "black") # inconclusive profits
         # Referenced this on how to make a histogram using with pre-grouped data
         # https://matplotlib.org/stable/api/_as_gen/matplotlib.pyplot.bar.html
-        plt.show()
-        # plt.savefig('test.png')
+        plt.plot(rating_groups[:-1]+ 0.25, grouped['entries_perc_times_200'], color = 'red') # entries percentage scaled by 200
+
+        plt.legend(['Percentage of Movies Per Group Scaled by 15', 'Profits in Millions', 'Rating Groups With Less Than 40 Entries (Inconclusive)'], fontsize = 12)
+
+        if show:
+            plt.show()
+        else:
+            plt.savefig('Movies_Ratings_vs_Median_Profits.png')
     
+    def PrintMedianRunTime(self):
+        print(f'Median Run Time: {self.data['runtimeMinutes'].median()}')
+    
+    def PrintMedianProfit(self):
+        print(f'Median Profit: {self.data['profit'].median()}')
+
+    def PrintMedianRating(self):
+        print(f'Median Rating: {self.data['averageRating'].median()}')
+
+    def PrintMeanRating(self):
+        print(f'Mean Rating: {self.data['averageRating'].mean()}')
+
+    def PrintStandardDeviationRating(self):
+        print(f'Standard Deviation Rating: {self.data['averageRating'].std()}')
+
     def RunTime_vs_Profitability(self, show):
         plt.figure(figsize=(8,5))
-        plt.title('Movies Runtime vs Median Profitability')
-        plt.xlabel('Run Time')
-        plt.ylabel('Profitability in millions')
-        # plt.scatter(movies.data['runtimeMinutes'], movies.data['profit'], color = 'blue', alpha = 0.5)
-
+        plt.title('Movies Run Time vs Median Profits', fontsize=16)
+        plt.xlabel('Run Time', fontsize=14)
+        plt.ylabel('Dollars in Millions', fontsize=14)
+        
+        ## LOESS PROFITS
         temp = self.data[['runtimeMinutes', 'profit']].copy(deep = True)
-        print(self.data['profit'].median())
-        print(self.data['runtimeMinutes'].median())
 
         grouped = temp.groupby('runtimeMinutes')['profit'].median().reset_index() 
         grouped = temp.groupby('runtimeMinutes').agg(
             median_profit = pd.NamedAgg(column='profit', aggfunc='median'),
-            entries = pd.NamedAgg(column='profit', aggfunc='count')
+            entries = pd.NamedAgg(column='profit', aggfunc='count'),
         ).reset_index() 
-        grouped = grouped[grouped['entries'] >= 20] # drop groupings less than 10 as data may get skewed by outliers
+        grouped = grouped[grouped['entries'] >= 20] 
+        # drop groupings less than 20 as data may get skewed by outliers
 
-        # plt.plot(grouped['runtimeMinutes'], grouped['median_profit'], color = 'red')
-
-        # median on profit column, reset index so I can access runTime colum
-        pd.set_option('display.max_rows', None)
-        display(grouped)
-
-        loess = lowess(grouped['median_profit'], grouped['runtimeMinutes'], frac = 0.1)
+        loess = lowess(grouped['median_profit'], grouped['runtimeMinutes'], frac = 0.1, it = 5)
         plt.plot(loess[:, 0], loess[:, 1], 'g-')
+
+        ## LINEAR REGRESSION
+        linspaceRunTime = np.linspace(75,155, 81)
+        runTime = pd.DataFrame(linspaceRunTime, columns=["runtimeMinutes"])
+        runTime = pd.merge(runTime, grouped, on = "runtimeMinutes", how="outer")
+        # outer since we want to keep linspace times
+
+        # -- Can uncomment to see which data points are imputed --
+        # pd.set_option('display.max_rows', None)
+        # display(runTime)
+
+        imputerKNN = KNNImputer(n_neighbors=5) 
+        # 5 neighbours since a lot of variance in neighbours of data
+        runTime = pd.DataFrame(imputerKNN.fit_transform(runTime), columns=runTime.columns)
+
+        fit = stats.linregress(runTime['runtimeMinutes'], runTime['median_profit'])
+        plt.plot(runTime['runtimeMinutes'], runTime['runtimeMinutes'] *fit.slope + fit.intercept, color = 'red', linewidth = 1)
+
+        plt.legend(['Profits', 'Linear Fit'], fontsize = 12)
 
         if (show):
             plt.show()
         else:
-            plt.savefig('runtime_vs_profits_median_cleaned.png')
+            plt.savefig('Movies_Run_Time_vs_Median_Profits.png')
+
+    def PrintMajorityOfDataInRange(self):
+        lower_bound = self.data['averageRating'].mean() - self.data['averageRating'].std()
+        upper_bound = self.data['averageRating'].mean() + self.data['averageRating'].std()
+        print(f'Approximately 68% of data is in the Rating range of : [{lower_bound:.2f}, {upper_bound:.2f}]')
+        # Referenced this for how to limit a number to 2 decimal places
+        # https://stackoverflow.com/questions/455612/limiting-floats-to-two-decimal-points
 
 if __name__ == "__main__":
     movies = MovieFactors()
     # movies.scatter_plot_ratings_vs_profitability()
-    movies.RunTime_vs_Profitability(True)
-    # movies.scatter_plot('Movies Ratings vs Profitability', 'Rating', 'Profitability in millions', 
-    #                     movies.data['averageRating'], movies.data['profit'], 
-    #                     'linearRating', 'averageRating', movies.data['profit'] , True, False, True )
-    
-    # movies.data = movies.data[movies.data['runtimeMinutes'] <= 250]
-    # print (movies.data['runtimeMinutes'])
-    # movies.data = movies.data[movies.data['profit'] <= 500]
 
-    # TODO
-    # maybe do loess smoothing or something?
-
-    
-
-    # movies.scatter_plot('Movies Runtime vs Profitability', 'Run Time', 'Profitability in millions', 
-    #                     movies.data['runtimeMinutes'], movies.data['profit'], 
-    #                     'linearRuntime', 'runtimeMinutes', movies.data['profit'] , False, True, True )
-    # movies.data['linearRuntime'] = movies.data['runtimeMinutes']
-    # movies.data['linearRating'] = movies.data['averageRating']
-    # movies.data['profit_in_millions'] = movies.data['profit'] 
-    # fit = stats.linregress(movies.data['linearRuntime'], movies.data['profit_in_millions'])
-    # print(fit)
-
-    # movies.histogram_ratings_vs_profitability()
-    # display(movies.data.sort_values(by="startYear"))
+    # movies.RunTime_vs_Profitability(show = True) # False will save png
+    movies.Histogram_Ratings_vs_Profits(show = True) # False will save png
+    movies.PrintMedianProfit()
+    movies.PrintMedianRunTime()
+    movies.PrintMedianRating()
+    movies.PrintMeanRating()
+    movies.PrintStandardDeviationRating()
+    movies.PrintMajorityOfDataInRange()
